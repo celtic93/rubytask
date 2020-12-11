0cee8a14ca4b8aa6cdca4b850d4114ecbef47a40
celtic93 <egorka80@list.ru>
2020-12-11 20:14:09 +0300

    Updated factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,27 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.com"
+  end
+
   factory :user do
-    
+    email
+    password { '12345678' }
+    password_confirmation { '12345678' }
+    login { 'login' }
+    fullname { 'fullname' }
+    address { 'address' }
+    city { 'city' }
+    state { 'state' }
+    country { 'country' }
+    zip { 'zip' }
+    birthday { Date.today }
+
+    trait :worker do
+      role { 1 }
+    end
+
+    trait :client do
+      role { 2 }
+    end
   end
 end
