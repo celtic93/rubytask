9555d207b6e132a630dd885b4055a92b8bbb0745
celtic93 <egorka80@list.ru>
2020-12-13 18:21:13 +0300

    Added UsersController with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth'
+
+  resources :users
 
   resources :tasks do
     resources :comments, shallow: true, only: %i(create update destroy)
