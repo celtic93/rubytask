f57f3bd73d827e7fd02e1aa4a16966c8a1a60b24
celtic93 <egorka80@list.ru>
2020-12-15 22:05:46 +0300

    Added file to task and comment models

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,9 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :worker, class_name: 'User', optional: true
 
+  has_one_attached :image
+
   validates :body, presence: true
+  validates :image, size: { less_than: 1.megabyte , message: 'is not given between size' },
+                    content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Comment, type: :model do
   it { should belong_to(:task) }
 
   it { should validate_presence_of :body }
+  it { should validate_content_type_of(:image).allowing('image/png', 'image/jpg', 'image/jpeg') }
+  it { should validate_size_of(:image).less_than(1.megabyte) }
+
+  it 'has one attached image' do
+    expect(Comment.new.image).to be_an_instance_of(ActiveStorage::Attached::One)
+  end
 end
