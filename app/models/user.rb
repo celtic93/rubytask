3e1da9da3d55557f708d1f7e97ad392267d192af
celtic93 <egorka80@list.ru>
2020-12-16 22:44:52 +0300

    Configured geocode

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,20 @@ class User < ApplicationRecord
   validates :login, :fullname, :birthday, :address, :city,
             :state, :country, :zip, :role, presence: true
 
+  geocoded_by :full_address
+  after_validation :geocode, if: :full_address_changed?
+
   def author?(resource)
     id == resource.user_id
+  end
+
+  private
+
+  def full_address
+    [address, city, state, country, zip].compact.join(', ')
+  end
+
+  def full_address_changed?
+    address_changed? || city_changed? || state_changed? || country_changed? || zip_changed?
   end
 end
